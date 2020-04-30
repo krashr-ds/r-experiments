d76fe3d0cd9b94d6de727321f176ebd5cf28da0c
Kylie R. RN <58571706+krashr-ds@users.noreply.github.com>
2020-04-30 15:53:51 -0400

    Update add_regression_line_ex.r

--- add_regression_line_ex.r
+++ add_regression_line_ex.r
@@ -1,6 +1,7 @@
 # Your plot
 plot(money, prosocial, xlab = "Money", ylab = "Prosocial Behavior")
 # Store your regression coefficients in a variable called "line"
-line <- lm(money ~ prosocial)
+# ALWAYS put the y variable FIRST
+line <- lm(prosocial ~ money)
 # Use "line" to tell abline() to make a line on your graph
 abline(line)
