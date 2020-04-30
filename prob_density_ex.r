81735df62bf7a6b0c2016bbde98293ae93f18281
Kylie R. RN <58571706+krashr-ds@users.noreply.github.com>
2020-04-30 19:53:54 -0400

    Create prob_density_ex.r

---
+++ prob_density_ex.r
@@ -0,0 +1,12 @@
+# simulating data
+set.seed(11225)
+data <- rnorm(10000)
+
+# check for documentation of the dnorm function
+help(dnorm)
+
+# calculate the density of data and store it in the variable density
+density <- dnorm(data)
+
+# make a plot with as x variable data and as y variable density
+plot(data, density)
