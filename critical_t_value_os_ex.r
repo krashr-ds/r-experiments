d77e6e53de10126e160b9bf03c8d483cab9cf042
Kylie R. RN <58571706+krashr-ds@users.noreply.github.com>
2020-04-30 20:33:40 -0400

    Create critical_t_value_os_ex.r

---
+++ critical_t_value_os_ex.r
@@ -0,0 +1,8 @@
+# COMPARING MEANS OF CONTINUOUS VARIABLES
+# CRITICAL VALUE FOR 95% ONE SIDED HYPOTHESIS TEST
+
+# calculate the critical cut off value and store it in a variable called cut_off
+cut_off <- round(qt(0.95,49),2)
+
+# print cut_off to the console
+cut_off
