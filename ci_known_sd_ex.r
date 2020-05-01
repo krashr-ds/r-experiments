1e437d11e13eab62ad546fcb92abe966633aec62
Kylie R. RN <58571706+krashr-ds@users.noreply.github.com>
2020-04-30 20:36:59 -0400

    Update ci_known_sd_ex.r

--- ci_known_sd_ex.r
+++ ci_known_sd_ex.r
@@ -25,3 +25,14 @@ m + (1.9720 * s)
 
 # Calculate the lower 95% confidence interval
 m - (1.9720 * s)
+
+# ANOTHER EXAMPLE
+# calculate the t value and store it in the variable t_value
+t_value <- round(qt(.975,49),2)
+
+#' calculate a 95% confidence interval as a vector with two values and store it in a
+#' a variable called conf_interval
+conf_interval <- c(round(186.5-0.71*t_value,2),round(186.5+0.71*t_value,2))
+
+# print conf_interval to the console
+conf_interval
