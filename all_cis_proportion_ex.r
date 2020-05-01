f699d6f5513797d42a779eb05865e3d5beb93d7b
Kylie R. RN <58571706+krashr-ds@users.noreply.github.com>
2020-04-30 20:18:18 -0400

    Update all_cis_proportion_ex.r

--- all_cis_proportion_ex.r
+++ all_cis_proportion_ex.r
@@ -12,3 +12,18 @@ p - 1.96 * se
 0.1766405
 # Which has the widest range?
 99
+
+# Assign the sample mean to object "m"
+m <- mean(rrage)
+# Assign the sample standard error to object "s"
+s <- sd(rrage) / sqrt(200)
+# Calculate the upper level of the 95% confidence interval
+m + ( 1.9720 * s )
+# Calculate the lower level of the 95% confidence interval
+m - ( 1.9720 * s)
+# Calculate the range of the 95% confidence interval
+(m + ( 1.9720 * s )) - (m - ( 1.9720 * s))
+# Calculate the range of the 90% confidence interval
+(m + ( 1.6525 * s )) - (m - ( 1.6525 * s))
+# Which has the widest range?
+95
