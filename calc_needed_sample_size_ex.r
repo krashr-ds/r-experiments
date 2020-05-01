fc8fa4bf8e9865ac8827dd791860d8b46cbc1afa
Kylie R. RN <58571706+krashr-ds@users.noreply.github.com>
2020-04-30 20:20:16 -0400

    Update calc_needed_sample_size_ex.r

--- calc_needed_sample_size_ex.r
+++ calc_needed_sample_size_ex.r
@@ -6,3 +6,13 @@ zs <- 1.96 ^ 2
 ms <- 0.2 ^ 2
 # Calculate the neccessary sample size
 ( ss * zs ) / ms
+
+
+# Assign the value of p(1-p) to object "p"
+p <- 0.25
+# Assign the value of the Z-score to new object "z"
+z <- 1.96
+# Assign the value of the margin of error squared to the new object "ms"
+ms <- .05^2
+# Calculate the neccessary sample size
+(p * z^2) / ms
