e5d20fc6e5f9d6ed2a9295d1b27eaeb216821f51
Kylie R. RN <58571706+krashr-ds@users.noreply.github.com>
2020-04-30 20:04:01 -0400

    Create area_under_curve_ex.r

---
+++ area_under_curve_ex.r
@@ -0,0 +1,4 @@
+# calculate the area under the curve left of the observation
+pnorm(2.02, lower.tail=TRUE)
+# calculate the area under the curve right of the observation
+pnorm(2.02, lower.tail=FALSE)
