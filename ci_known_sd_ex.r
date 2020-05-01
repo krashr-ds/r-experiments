3965984a7cd7396edc7c70f90864efa257c43da5
Kylie R. RN <58571706+krashr-ds@users.noreply.github.com>
2020-04-30 20:12:59 -0400

    Update ci_known_sd_ex.r

--- ci_known_sd_ex.r
+++ ci_known_sd_ex.r
@@ -1,3 +1,5 @@
+# USING KNOWN POPULATION SD (ALMOST NEVER) 
+
 # Assign the sample mean to object "m"
 m <- mean(samp)
 
@@ -9,3 +11,17 @@ m + ( 1.96 * s )
 
 # Calculate the lower confidence interval
 m - ( 1.96 * s )
+
+# USING SAMPLE SD
+
+# Assign the sample mean to object "m"
+m = mean(rrage)
+
+# Assign the sample standard error to object "s"
+s =  sd(rrage) / sqrt(200)
+
+# Calculate the upper 95% confidence interval
+m + (1.9720 * s)
+
+# Calculate the lower 95% confidence interval
+m - (1.9720 * s)
