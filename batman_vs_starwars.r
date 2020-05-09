1403f1fc6991afecd50f7ef070fa37baf996bba6
krashr-ds <kprasku@tutanota.com>
2020-05-08 22:42:02 -0400

    Update batman_vs_starwars.r

--- batman_vs_starwars.r
+++ batman_vs_starwars.r
@@ -14,5 +14,13 @@ star_wars_colors <- inventory_parts_themes %>%
   group_by(color_id) %>%
   summarize(total = sum(quantity)) %>%
   mutate(percent = total / sum(total))
-  
-  
+ 
+batman_colors %>%
+  full_join(star_wars_colors, by = "color_id", suffix = c("_batman", "_star_wars")) %>%
+  replace_na(list(total_batman = 0, total_star_wars = 0)) %>%
+  inner_join(colors, by = c("color_id" = "id")) %>%
+  # Create the difference and total columns
+  mutate(difference = percent_batman - percent_star_wars,
+         total = total_batman + total_star_wars) %>%
+  # Filter for totals greater than 200
+  filter(total >= 200)
