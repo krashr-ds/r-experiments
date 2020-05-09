9fc47504a437c91bb72af39c12afbb6a4d606d93
krashr-ds <kprasku@tutanota.com>
2020-05-08 22:30:52 -0400

    Create batman_vs_starwars.r

---
+++ batman_vs_starwars.r
@@ -0,0 +1,18 @@
+# DataCamp Exercise Comparing Lego Themes
+
+batman_colors <- inventory_parts_themes %>%
+  # Filter the inventory_parts_themes table for the Batman theme
+  filter(name_theme == "Batman") %>%
+  group_by(color_id) %>%
+  summarize(total = sum(quantity)) %>%
+  # Add a percent column of the total divided by the sum of the total 
+  mutate(percent = total / sum(total))
+
+# Filter and aggregate the Star Wars set data; add a percent column
+star_wars_colors <- inventory_parts_themes %>%
+  filter(name_theme == "Star Wars") %>%
+  group_by(color_id) %>%
+  summarize(total = sum(quantity)) %>%
+  mutate(percent = total / sum(total))
+  
+  
