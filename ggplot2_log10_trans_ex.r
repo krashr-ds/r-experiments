e9592cf28d99ddb9d18ba6b01f9fce4c5c201e8d
krashr-ds <kprasku@tutanota.com>
2020-05-21 22:01:06 -0400

    Create ggplot2_log10_trans_ex.r

---
+++ ggplot2_log10_trans_ex.r
@@ -0,0 +1,6 @@
+# Plot with transformed coordinates
+ggplot(msleep, aes(bodywt, brainwt)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  # Add a log10 coordinate transformation for x and y axes
+  coord_trans(x="log10", y="log10")
