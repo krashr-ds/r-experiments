a6eafb332ece079d69addcb4eb38ab694ccb2ab0
krashr-ds <kprasku@tutanota.com>
2020-05-25 15:06:12 -0400

    Create ggplot2_clearplot_ex.r

---
+++ ggplot2_clearplot_ex.r
@@ -0,0 +1,19 @@
+# Change type
+TG$dose <- as.numeric(as.character(TG$dose))
+
+# Plot
+growth_by_dose <- ggplot(TG, aes(dose, len, color = supp)) +
+  stat_summary(fun.data = mean_sdl,
+               fun.args = list(mult = 1),
+               position = position_dodge(0.2)) +
+  stat_summary(fun.y = mean,
+               geom = "line",
+               position = position_dodge(0.1)) +
+  theme_classic() +
+  # Adjust labels and colors:
+  labs(x = "Dose (mg/day)", y = "Odontoblasts length (mean, standard deviation)", color = "Supplement") +
+  scale_color_brewer(palette = "Set1", labels = c("Orange juice", "Ascorbic acid")) +
+  scale_y_continuous(limits = c(0,35), breaks = seq(0, 35, 5), expand = c(0,0))
+
+# View plot
+growth_by_dose
