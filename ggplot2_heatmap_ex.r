fea965d85dd6f5d5030fa25241c55db00dcc6a77
krashr-ds <kprasku@tutanota.com>
2020-05-25 11:14:30 -0400

    Create ggplot2_heatmap_ex.r

---
+++ ggplot2_heatmap_ex.r
@@ -0,0 +1,9 @@
+# A palette of 9 reds
+red_brewer_palette <- brewer.pal(9, "Reds")
+
+# Update the plot
+ggplot(barley, aes(x = year, y = variety, fill = yield)) +
+  geom_tile() + 
+  facet_wrap(facets = vars(site), ncol = 1) +
+  # Update scale to use n-colors from red_brewer_palette
+  scale_fill_gradientn(colors = red_brewer_palette)
