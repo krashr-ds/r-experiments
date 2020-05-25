d0e007c359296aeb8944517036d8cbf13f940177
krashr-ds <kprasku@tutanota.com>
2020-05-25 07:24:42 -0400

    Create ggplot_meansderrorbar_ex.r

---
+++ ggplot_meansderrorbar_ex.r
@@ -0,0 +1,6 @@
+# Plot wt vs. fcyl
+ggplot(mtcars, aes(x = fcyl, y = wt)) +
+  # Add a bar summary stat of means, colored skyblue
+  stat_summary(fun.y = mean, geom = "bar", fill = "skyblue") +
+  # Add an errorbar summary stat std deviation limits
+  stat_summary(fun.data = mean_sdl, fun.args = list(mult = 1), geom = "errorbar", width = 0.1)
