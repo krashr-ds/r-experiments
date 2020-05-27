6b6e868772da21f6500e97302b03e23d94653141
krashr-ds <kprasku@tutanota.com>
2020-05-27 19:17:32 -0400

    Create ggplot2_boxplotvdensity_ex.r

---
+++ ggplot2_boxplotvdensity_ex.r
@@ -0,0 +1,11 @@
+# Boxplot for many outliers; Density plot for significant variation
+
+# Create plot of city_mpg
+cars %>%
+  ggplot(aes(x = 1, y = city_mpg)) +
+  geom_boxplot()
+
+# Create plot of width
+cars %>% 
+  ggplot(aes(x = width)) +
+  geom_density()
