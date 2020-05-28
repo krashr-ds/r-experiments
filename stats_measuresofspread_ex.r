f3aa5c14d87f5ec3619a1b3fde39ebb72e26f008
krashr-ds <kprasku@tutanota.com>
2020-05-27 21:33:25 -0400

    Update stats_measuresofspread_ex.r

--- stats_measuresofspread_ex.r
+++ stats_measuresofspread_ex.r
@@ -9,3 +9,17 @@ gap2007 %>%
 gap2007 %>%
   ggplot(aes(x = lifeExp, fill = continent)) +
   geom_density(alpha = 0.3)
+
+
+# Compute stats for lifeExp in Americas
+# This is a relatively normally-distributed curve
+gap2007 %>%
+  filter(continent == "Americas") %>%
+  summarize(mean(lifeExp),
+            sd(lifeExp))
+
+# Compute stats for population
+# This is a heavily skewed curve with many outliers 
+gap2007 %>%
+  summarize(median(pop),
+            IQR(pop))
